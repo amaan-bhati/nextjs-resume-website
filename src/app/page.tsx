a098aaeb86fa4d4d52f57a40d0c97684b493b148
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-07-11 17:35:06 +0530

    try production bug fix-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,7 +163,7 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
+                  I've worked on a variety of projects, from simple
                   websites to complex web applications. Here are a few of my
                   favorites.
                 </p>
