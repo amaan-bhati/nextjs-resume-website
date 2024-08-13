2c65726aa5c9160c27bae4d1188ef7cb272a581e
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-08-14 01:51:24 +0530

    feat: add-my-image

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,12 @@ export default function Page() {
                 text={DATA.description}
               />
             </div>
-            {/* <BlurFade delay={BLUR_FADE_DELAY}>
+            <BlurFade delay={BLUR_FADE_DELAY}>
               <Avatar className="size-28 border">
                 <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
-            </BlurFade> */}
+            </BlurFade>
           </div>
         </div>
       </section>
