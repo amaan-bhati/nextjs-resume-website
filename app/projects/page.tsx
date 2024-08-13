0347a3759ae2e881abb73d25874202b6521f94a2
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-08-14 01:40:15 +0530

    feat: add-projects-page

---
+++ app/projects/page.tsx
@@ -0,0 +1,54 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import { ProjectCard } from "@/components/project-card";
+// import { ResumeCard } from "@/components/resume-card";
+import { DATA } from "@/data/resume";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-3">
+       <section id="projects">
+        <div className="space-y-10 w-full py-0">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-3 text-center">
+              
+                
+                <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-3xl">
+                    Check out my work
+                  </h2>
+                </div>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve lead, made and worked on a variety of projects, from
+                  simple websites to complex web applications. Here are a few of
+                  my favorites.
+                </p>
+              
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
