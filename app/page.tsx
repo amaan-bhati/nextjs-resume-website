681c64cd22d75e6cdb8d25fbe9174a4340fb0edc
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-09-12 23:14:24 +0530

    UI: update-project-section-ui_add-lead-projects-component

--- app/page.tsx
+++ app/page.tsx
@@ -153,14 +153,55 @@ export default function Page() {
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  My Projects
-                </div>
-                <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="flex flex-col items-center justify-center space-y-4 text-center">
                   <h2 className="text-3xl font-bold tracking-tighter sm:text-3xl">
                     Check out my work
                   </h2>
                 </div>
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Personal Projects
+                </div>
+                
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve made and worked on a variety of projects, from
+                  simple websites and landing pages to complex web applications. Here are a few of
+                  my favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Lead/Group Projects
+                </div>
+                
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I&apos;ve lead, made and worked on a variety of projects, from
                   simple websites to complex web applications. Here are a few of
@@ -190,7 +231,7 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
       {/* <section id="hackathons">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
