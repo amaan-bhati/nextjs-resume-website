9fec3fc2533e77aad2a3aeef930f00585c5bfa31
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-28 22:00:42 +0530

    optimisation: try-performance-optimisation-by-removing-hero-images

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,6 @@ export default function Page() {
                 yOffset={8}>
                   <p className="text-s sm:text-xs">Building creative and interactive UI in a <Cover>FLASH</Cover></p>
                 </BlurFade> */}
-
               <BlurFade
                 delay={0}
                 className="text-7xl font-bold tracking-tighter sm:text-6xl xl:text-7xl/none"
@@ -129,7 +128,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section id="images" className="py-1">
+      {/* <section id="images" className="py-1">
         <BlurFade delay={BLUR_FADE_DELAY * 3}>
           <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
             <div className="overflow-hidden rounded-lg border">
@@ -159,7 +158,7 @@ export default function Page() {
             </div>
           </div>
         </BlurFade>
-      </section>
+      </section> */}
 
       <section id="about">
         <BlurFade delay={BLUR_FADE_DELAY * 3}>
