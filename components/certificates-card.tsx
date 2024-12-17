95e2266a363aeee11b21baa5c920c6eaed8d3ce7
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-12-17 21:36:05 +0530

    feat: add-footer-to-certificates-card-component_finish-component

--- components/certificates-card.tsx
+++ components/certificates-card.tsx
@@ -95,6 +95,20 @@ export function certificatesCard({
           </div>
         )}
       </CardContent>
+      <CardFooter className="px-2 pb-2">
+        {links && links.length > 0 && (
+          <div className="flex flex-row flex-wrap items-start gap-1">
+            {links?.map((link, idx) => (
+              <Link href={link?.href} key={idx} target="_blank">
+                <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
+                  {link.icon}
+                  {link.type}
+                </Badge>
+              </Link>
+            ))}
+          </div>
+        )}
+      </CardFooter>
     
      
     </Card>
