227eedfb5029ec684e04b5c9a174868b29877cf7
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-12-17 21:35:08 +0530

    ui: add-card-header-and-card-content-ft-to-certificates-card-component

--- components/certificates-card.tsx
+++ components/certificates-card.tsx
@@ -68,6 +68,34 @@ export function certificatesCard({
           />
         )}
       </Link>
+      <CardHeader className="px-2">
+        <div className="space-y-1">
+          <CardTitle className="mt-1 text-base">{title}</CardTitle>
+          <time className="font-sans text-xs">{dates}</time>
+          <div className="hidden font-sans text-xs underline print:visible">
+            {link?.replace("https://", "").replace("www.", "").replace("/", "")}
+          </div>
+          <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
+            {description}
+          </Markdown>
+        </div>
+      </CardHeader>
+      <CardContent className="mt-auto flex flex-col px-2">
+        {tags && tags.length > 0 && (
+          <div className="mt-2 flex flex-wrap gap-1">
+            {tags?.map((tag) => (
+              <Badge
+                className="px-1 py-0 text-[10px]"
+                variant="secondary"
+                key={tag}
+              >
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        )}
+      </CardContent>
+    
      
     </Card>
   );
